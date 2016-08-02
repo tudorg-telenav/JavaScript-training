ddfa7a5232995cd8fb8a42e49a0eaf0262bc1675
tudor.goia <tudor.goia@telenav.com>
2016-08-02 19:49:37 +0300

    changes from June 14, 2016 - synch vs asynch

--- script.js
+++ script.js
@@ -1,64 +1,17 @@
+var fs = require('fs');
 
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('test.xml')
-});
+var rezultat1 = fs.readFileSync( __dirname + '/test.xml');
 
-var i = 1;
-lineReader.on('line', function (line) {
-  
-  
-  
-  if (i == 4) {
-    
-    require('fs').appendFile(
-      __dirname + '/rezultat.xml',
-      line,
-      {
-        flag: 'a'
-      },
-      function (err) {
-      }
-    );
-    
-    console.log('Line from file:', line);
+console.log(typeof rezultat1);
+console.log(rezultat1.toString());
+
+// ------------------------------
+var rezultat2 = fs.readFileSync(
+  __dirname + '/test.xml',
+  {
+    encoding: 'utf8'
   }
-  i++;
-});
+  );
 
-
-// TODO: construim tot programul ca parte a unui obiect ca sa nu nest-uim functiile
-
-var CititorLinie = function(numeFisier, nrLinie) {
-  this.numeFisier = numeFisier;
-  this.nrLinie = nrLinie;
-  
-  this.interfataFisier = require('readline').createInterface({
-    input: require('fs').createReadStream(numeFisier)
-  });
-  
-  this.rezultat = function(unCallback) {
-    var i = 1;
-    this.interfataFisier.on('line', function (line) {
-
-      if (i == nrLinie) {
-        console.log('Line from file:', line);
-      }
-      i++;
-    });
-    
-    
-    
-    return 'nimic';
-  }
-}
-
-var cititor1 = new CititorLinie('test.xml', 4);
-
-var cititor2 = new CititorLinie('rezultat.xml', 2);
-
-cititor1.rezultat(function(rezultat) {
-  console.log(rezultat);
-});
-cititor2.rezultat(function(rezultat) {
-  console.log(rezultat);
-});
+console.log(typeof rezultat2);
+console.log(rezultat2);
