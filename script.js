22de431fc1de48db57b52c87abb029497d82b25f
tudor.goia <tudor.goia@telenav.com>
2016-08-02 19:57:35 +0300

    changes from July 12, 2016 - bounding boxes

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ var fs = require('fs');
 var uuid = require('node-uuid');
 
 
+
+var TOTAL_ITEMS = 5;
+
+var WORLD_MIN_LAT = -90;
+var WORLD_MAX_LAT = 90;
+var WORLD_MIN_LON = -180;
+var WORLD_MAX_LON = 180;
+
+var ITEM_MIN_SIZE = 0.01;
+var ITEM_MAX_SIZE = 0.1;
+
+var MAX_CORNERS = 8;
+var MIN_CORNERS = 1;
+
+
+
 var ReprezentareFisierJSON = function(obiectBaza, indexFisier) {
     this.obiectBaza = obiectBaza;
     this.indexFisier = indexFisier;
@@ -15,22 +31,20 @@ var ReprezentareFisierJSON = function(obiectBaza, indexFisier) {
     }
 }
 
+var randomFromRange = function(min, max) {
+    return ((max - min) * Math.random()) + min;
+};
 
 var obiecte = [];
 
-for (var i = 0; i <= 4; i++) {
-
-console.log(Date.now())
+for (var i = 0; i < TOTAL_ITEMS; i++) {
 
     var obj1 = {
         insightId: uuid.v4(),
         startTime: Date.now() - (2 * 86400000),
         endDate: Date.now() - 86400000,
-        a: 2,
-        b: 'dasfas',
-        c: {
-            ee: 4
-        }
+        lat: randomFromRange(WORLD_MIN_LAT, WORLD_MAX_LAT),
+        lon: randomFromRange(WORLD_MIN_LON, WORLD_MAX_LON)
     };
 
     obiecte[i] = new ReprezentareFisierJSON(
