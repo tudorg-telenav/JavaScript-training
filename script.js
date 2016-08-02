dd67777a5e5d5a959e9be635c9be4e7ddddd4fb7
tudor.goia <tudor.goia@telenav.com>
2016-08-02 19:56:11 +0300

    changes from July 7, 2016 - real use case json

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-var obj1 = {
-    a: 2,
-    b: 'dasfas',
-    c: {
-        ee: 4
-    }
-};
-
-
 var fs = require('fs');
+var uuid = require('node-uuid');
 
 
 var ReprezentareFisierJSON = function(obiectBaza, indexFisier) {
@@ -28,6 +20,19 @@ var obiecte = [];
 
 for (var i = 0; i <= 4; i++) {
 
+console.log(Date.now())
+
+    var obj1 = {
+        insightId: uuid.v4(),
+        startTime: Date.now() - (2 * 86400000),
+        endDate: Date.now() - 86400000,
+        a: 2,
+        b: 'dasfas',
+        c: {
+            ee: 4
+        }
+    };
+
     obiecte[i] = new ReprezentareFisierJSON(
         obj1,
         i
@@ -36,5 +41,3 @@ for (var i = 0; i <= 4; i++) {
     obiecte[i].creazaFisier();
 
 }
-
-console.log(obiecte);
