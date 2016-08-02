f2c71fe1b01ad189af400ead6540bc00be62e9e9
tudor.goia <tudor.goia@telenav.com>
2016-08-02 20:02:06 +0300

    changes from July 26, 2016 - TODOs + polygons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// TODO: we have to remember to cover the case for
+// 3 points (first = last) describing a line
+
+// TODO: explore command line options
+
+// TODO: git versioning and backward commits for each day
+
+// TODO: presentation @ all hands meeting
+
+
+
+
 var fs = require('fs');
 var uuid = require('node-uuid');
 
@@ -13,19 +25,22 @@ var WORLD_MAX_LON = 180;
 var ITEM_MIN_RADIUS = 0.01;
 var ITEM_MAX_RADIUS = 0.1;
 
-var MAX_CORNERS = 8;
-var MIN_CORNERS = 1;
+var MAX_CORNERS = 7;
+var MIN_CORNERS = 2;
 
 
 
-var ReprezentareFisierJSON = function(obiectBaza, indexFisier) {
+var ReprezentareFisierJSON = function(
+    obiectBaza,
+    indexFisier
+) {
     this.obiectBaza = obiectBaza;
     this.indexFisier = indexFisier;
-    this.numeFisier = __dirname + '/insight' + indexFisier + '.json';
+    this.getProximityPointFisier = __dirname + '/insight' + indexFisier + '.json';
     
     this.creazaFisier = function() {
         fs.writeFileSync(
-            this.numeFisier,
+            this.getProximityPointFisier,
             JSON.stringify(this.obiectBaza, null, 4)
         );
     }
@@ -43,12 +58,35 @@ var ItemGenerator = function() {
     };
 
     this.getWorldLat = function() {
-        return this.randomFromRange(WORLD_MIN_LAT, WORLD_MAX_LAT);
+        return this.randomFromRange(
+            WORLD_MIN_LAT,
+            WORLD_MAX_LAT
+        );
     };
 
     this.getWorldLon = function() {
-        return this.randomFromRange(WORLD_MIN_LON, WORLD_MAX_LON);
+        return this.randomFromRange(
+            WORLD_MIN_LON,
+            WORLD_MAX_LON
+        );
     };
+
+    this.getProximityPoint = function(basePoint, radius) {
+        return {
+            lat: this.randomFromRange(
+                basePoint.lat - radius,
+                basePoint.lat + radius
+            ),
+            lon: this.randomFromRange(
+                basePoint.lon - radius,
+                basePoint.lon + radius
+            )
+        };
+    }
+
+    this.getSquare = function(point1, point2) {
+
+    }
 
     // main out API function
     this.getNewItem = function() {
@@ -58,23 +96,60 @@ var ItemGenerator = function() {
             lon: this.getWorldLon()
         };
 
-        var affectedCornersNo = this.randomIntFromRange(MIN_CORNERS, MAX_CORNERS);
-        var distributionCornersNo = this.randomIntFromRange(MIN_CORNERS, MAX_CORNERS);
+        var distributionCornersNo = this.randomIntFromRange(
+            MIN_CORNERS,
+            MAX_CORNERS
+        );
+        var affectedCornersNo = this.randomIntFromRange(
+            MIN_CORNERS,
+            MAX_CORNERS
+        );
+
+        var radius = this.randomFromRange(
+            ITEM_MIN_RADIUS,
+            ITEM_MAX_RADIUS
+        );
+
+        var distributionCorners = [];
+        for (var i = 0; i < distributionCornersNo; i++) {
+            distributionCorners[i] = this.getProximityPoint(
+                basePoint,
+                radius
+            );
+        }
+
+        if (distributionCornersNo > 2) {
+            distributionCorners.push(
+                distributionCorners[0]
+            );
+        } else {
+            distributionCorners = this.getSquare(
+                distributionCorners[0],
+                distributionCorners[1]
+            );
+        }
 
         var affectedCorners = [];
         for (var i = 0; i < affectedCornersNo; i++) {
-            affectedCorners[i] = {
-
-            };
+            affectedCorners[i] = this.getProximityPoint(
+                basePoint,
+                radius
+            );
+        }
+        if (affectedCornersNo > 2) {
+            affectedCorners.push(
+                affectedCorners[0]
+            );
+        } else {
+            affectedCorners = this.getSquare(
+                affectedCorners[0],
+                affectedCorners[1]
+            );
         }
 
         return {
-            affected: {
-                lat: this.getWorldLat()
-            },
-            distribution: {
-                lon: this.getWorldLon()
-            }
+            distribution: distributionCorners,
+            affected: affectedCorners
         }
     };
 
@@ -91,8 +166,8 @@ for (var i = 0; i < TOTAL_ITEMS; i++) {
         insightId: uuid.v4(),
         startTime: Date.now() - (2 * 86400000),
         endDate: Date.now() - 86400000,
-        affectedArea: poligonPairItem.affected,
-        distributionArea: poligonPairItem.distribution
+        distributionArea: poligonPairItem.distribution,
+        affectedArea: poligonPairItem.affected
     };
 
     obiecte[i] = new ReprezentareFisierJSON(
