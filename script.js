8d341cf99e66ee8b150b2f5f53e786726b580d91
tudor.goia <tudor.goia@telenav.com>
2016-08-02 19:45:33 +0300

    changes from May 25, 2016

--- script.js
+++ script.js
@@ -6,12 +6,27 @@ fs.readFile( __dirname + '/test.xml', function (err, data) {
   console.log(data.toString());
 });
 
-//==============================================
+console.log('==============================================');
 
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('test.xml')
 });
-
+var i = 0;
 lineReader.on('line', function (line) {
-  console.log('Line from file:', line);
+  
+  
+  
+  if (i == 4) {
+    console.log('Line from file:', line);
+    console.log(fs);
+  }
+  i++;
+  
+  
+  
+  
+  
 });
+
+
+// TODO: construim tot programul ca parte a unui obiect ca sa nu nest-uim functiile
